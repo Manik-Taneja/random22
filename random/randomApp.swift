6356888bb0360e72b21e2c568690febe0ddcd6d7
Manik Taneja <maniktaneja712@gmail.com>
2023-03-18 19:43:20 -0500

    Initial Commit

---
+++ random/randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomApp.swift
+//  random
+//
+//  Created by Manik Taneja on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
